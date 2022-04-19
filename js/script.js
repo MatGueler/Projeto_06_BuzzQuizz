dbcb812058bc1748d829c8fecc063791a53cc921
Mateus <mateus.gueler@gmail.com>
2022-04-19 16:58:49 -0300

    layout desktop e mobile pronto para tela principal

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function criarQuizz(){
+   let meusQuiz = document.querySelector(".meus-quizes")
+   meusQuiz.innerHTML = ""
+   meusQuiz.innerHTML += '<div class="caixa-quiz"><img src="https://d5y9g7a5.rocketcdn.me/wp-content/uploads/2020/04/bicho-preguica-caracteristicas-das-especies-e-curiosidades.jpg"><h2>Pergunta do quiz</h2><div class="caixa-gradiente"></div></div>'
+   console.log(meusQuiz.innerHTML)
+}
