e6407207d32e5351d3fb3112a78617eb26bb65e4
Lucas Lafèré <laferelcas@gmail.com>
2022-04-19 19:05:32 -0300

    comment no script.js como teste

--- js/script.js
+++ js/script.js
@@ -4,3 +4,5 @@ function criarQuizz(){
    meusQuiz.innerHTML += '<div class="caixa-quiz"><img src="https://d5y9g7a5.rocketcdn.me/wp-content/uploads/2020/04/bicho-preguica-caracteristicas-das-especies-e-curiosidades.jpg"><h2>Pergunta do quiz</h2><div class="caixa-gradiente"></div></div>'
    console.log(meusQuiz.innerHTML)
 }
+
+//testando se a gente sabe fazer um pull decentemente kkkkkkk
