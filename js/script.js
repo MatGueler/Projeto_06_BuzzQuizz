07380dd0b85b3937a774f37fd605055d72e27015
Mateus <mateus.gueler@gmail.com>
2022-04-20 01:20:54 -0300

    Layout tela1 e tela3 prontos

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 function criarQuizz(){
-   let meusQuiz = document.querySelector(".meus-quizes")
-   meusQuiz.innerHTML = ""
-   meusQuiz.innerHTML += '<div class="caixa-quiz"><img src="https://d5y9g7a5.rocketcdn.me/wp-content/uploads/2020/04/bicho-preguica-caracteristicas-das-especies-e-curiosidades.jpg"><h2>Pergunta do quiz</h2><div class="caixa-gradiente"></div></div>'
+   document.querySelector(".container-meus").classList.remove("desativar")
+   let meusQuizVazio = document.querySelector(".meus-quizes")
+   meusQuizVazio.classList.add("desativar")
+   let meuQuizCheio = document.querySelector(".quizes")
+   meuQuizCheio.classList.remove("desativar")
+
+   meuQuizCheio.innerHTML = ""
+   for(let contador = 0; contador<4;contador++){
+      meuQuizCheio.innerHTML += '<div class="caixa-quiz"><img src="https://d5y9g7a5.rocketcdn.me/wp-content/uploads/2020/04/bicho-preguica-caracteristicas-das-especies-e-curiosidades.jpg"><h2>Pergunta do quiz</h2><div class="caixa-gradiente"></div></div>'
+   }
    console.log(meusQuiz.innerHTML)
 }
-
-//testando se a gente sabe fazer um pull decentemente kkkkkkk
-
-// A gente sabe!! KKKKKKKKKK
